0b0b94c20aff8230a8515dac9627cb2c700f8e9c
rodolfoespinho <132661930+rodolfoespinho@users.noreply.github.com>
2026-03-11 16:17:28 +0000

    Add Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 2000,
-  }
-})
+export default defineConfig({ plugins: [react()], build: { outDir: 'dist', chunkSizeWarningLimit: 2000 } })
