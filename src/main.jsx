4ebf5560699a836fcfe0432c0f2fe7760b909d31
Viamar - Ilha da Berlenga <viamar@MacBook-Pro-de-Viamar.local>
2026-03-13 11:54:35 +0000

    add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import App from './App.jsx'
+
+injectSpeedInsights()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
